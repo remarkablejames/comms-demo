0a27743bccbff40a7045bcdef6203bef07c1fb11
James Niyongira <lascasasjames85@gmail.com>
2023-06-21 23:22:57 -0400

    created a program to add three numbers

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-       System.out.println("Hello World from another file!");
+       // get three numbers from the user and add them together
+         Scanner scanner = new Scanner(System.in);
+            int firstNumber = scanner.nextInt();
+            int secondNumber = scanner.nextInt();
+            int thirdNumber = scanner.nextInt();
+            int result = firstNumber + secondNumber + thirdNumber;
+            System.out.println(result);
     }
 }
