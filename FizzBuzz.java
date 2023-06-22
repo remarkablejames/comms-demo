35dafc93f28bc904d4cf3c6365efc101bdadf39f
James Niyongira <lascasasjames85@gmail.com>
2023-06-21 23:22:03 -0400

    displayed hello world

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,21 +2,6 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        if (number % 3 == 0 && number % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (number % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (number % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(number);
-        }
-
-        scanner.close();
+       System.out.println("Hello World from another file!");
     }
 }
