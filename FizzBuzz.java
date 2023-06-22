7ecdfbbd934b5b7617232b1844b885d270a555cf
James Niyongira <lascasasjames85@gmail.com>
2023-06-21 23:24:19 -0400

    deleted everything and left a comment

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-       // get three numbers from the user and add them together
-         Scanner scanner = new Scanner(System.in);
-            int firstNumber = scanner.nextInt();
-            int secondNumber = scanner.nextInt();
-            int thirdNumber = scanner.nextInt();
-            int result = firstNumber + secondNumber + thirdNumber;
-            System.out.println(result);
+       // this is a comment
     }
 }
